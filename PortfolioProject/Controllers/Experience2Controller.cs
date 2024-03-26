d46e8c51c36fcfea21969697b31b6b5761fb9e16
Tekin Öztürk <tekinozturk0@gmail.com>
2024-03-26 14:18:17 +0300

    ajax bitti

--- PortfolioProject/Controllers/Experience2Controller.cs
+++ PortfolioProject/Controllers/Experience2Controller.cs
@@ -2,6 +2,7 @@
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace PortfolioProject.Controllers
 {
@@ -34,5 +35,47 @@ namespace PortfolioProject.Controllers
             return Json(values);
 
         }
+
+        public IActionResult GetById(int ExperienceId)
+        {
+
+            var values = JsonConvert.SerializeObject(_experienceService.TGetById(ExperienceId));
+            return Json(values);
+
+        }
+
+        public IActionResult DeleteExperience(int id)
+        {
+            var values = _experienceService.TGetById(id);
+            _experienceService.TDelete(values);
+            return NoContent();
+        }
+
+        [HttpPost]
+        public IActionResult UpdateExperience(Experience e)
+        {
+
+            //var updatedExp = new Experience()
+            //{
+            //    ExperienceId = e.ExperienceId,
+            //    Name = e.Name,
+            //    Date = e.Date,
+            //    ImageUrl = e.ImageUrl,
+            //    Description = e.Description
+            //};
+
+            var values = _experienceService.TGetById(e.ExperienceId);
+            values.ExperienceId= e.ExperienceId;
+            values.Name= e.Name;
+            values.Date= e.Date;
+            values.ImageUrl= e.ImageUrl;
+            values.Description= e.Description;
+
+            _experienceService.TUpdate(values);
+            var values2 = JsonConvert.SerializeObject(values);
+            return Json(values2);
+
+        }
+
     }
 }
